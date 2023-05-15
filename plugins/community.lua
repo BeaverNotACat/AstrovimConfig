df1fee6593db400c77f9ace3f9b0b031a357a963
BeaverNotACat <beavernotacat@gmail.com>
2023-05-16 00:17:57 +1000

    ✨ Added markdown/LaTex plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,8 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
